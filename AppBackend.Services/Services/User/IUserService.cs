e911c69d3c79268d4126c62a3df82bc82c6d9911
toanno2709 <toanntse161543@fpt.edu.vn>
2025-10-27 23:09:42 +0700

    update api authentication

--- AppBackend.Services/Services/User/IUserService.cs
+++ AppBackend.Services/Services/User/IUserService.cs
@@ -5,10 +5,17 @@ namespace AppBackend.Services
 {
     public interface IUserService
     {
-<<<<<<< Updated upstream
-        Task<ResultModel> RegisterAsync(RegisterRequest request);
-        Task<ResultModel> LoginAsync(LoginRequest request);
+        #region User Profile
+        Task<ResultModel> GetCurrentUserAsync(int userId);
+        #endregion
+
+        #region Admin - User Management
         Task<ResultModel> GetAllUsersAsync();
         Task<ResultModel> GetUserByIdAsync(int id);
+        Task<ResultModel<UserResponseDto>> CreateUserAsync(CreateUserRequest request);
+        Task<ResultModel<UserResponseDto>> UpdateUserAsync(int userId, UpdateUserRequest request);
+        Task<ResultModel<List<UserResponseDto>>> GetUsersByRoleAsync(int? roleId);
+        Task<ResultModel<bool>> DeleteUserAsync(int userId);
+        #endregion
     }
 }
