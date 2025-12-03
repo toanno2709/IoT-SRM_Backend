75782aa26bac8b5310c536646a56468fc6082c3b
toanno2709 <toanntse161543@fpt.edu.vn>
2025-12-03 21:38:18 +0700

    add api create milestone for all project in class

--- AppBackend.Services/Services/ProjectMilestone/IProjectMilestoneService.cs
+++ AppBackend.Services/Services/ProjectMilestone/IProjectMilestoneService.cs
@@ -8,6 +8,13 @@ public interface IProjectMilestoneService
     Task<ResultModel<ProjectMilestoneResponseDto>> CreateAsync(ProjectMilestoneCreateRequestDto request);
     Task<ResultModel<ProjectMilestoneResponseDto>> UpdateAsync(ProjectMilestoneUpdateRequestDto request);
     Task<ResultModel<bool>> DeleteAsync(int milestoneId);
+    
+    /// <summary>
+    /// Create milestones for all approved projects in a class
+    /// </summary>
+    /// <param name="request">Bulk creation request containing milestone details</param>
+    /// <returns>Result with details of created milestones</returns>
+    Task<ResultModel<BulkCreateMilestoneResponseDto>> BulkCreateMilestoneForClassAsync(BulkCreateMilestoneRequestDto request);
 }
 
 
